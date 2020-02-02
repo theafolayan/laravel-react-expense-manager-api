0c4f3e1918d3210d95f8d0cd4eb00068e4480c91
theafolayan <theafolayan@gmail.com>
2020-02-02 03:08:38 +0100

    updated config/database.php file with remote mysql credentials

--- config/database.php
+++ config/database.php
@@ -45,12 +45,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => 'remotemysql.com',
+            'host' => 'remotemysql.com',
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => '43TSg5FI2Q',
+            'username' => '43TSg5FI2Q',
+            'password' => 'Kd1rYahAsW',
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
